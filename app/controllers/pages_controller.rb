d16e3b15fd21c47dc9f23a1b72e46c5f9ce55b8c
docmj <doughmj@outlook.com>
2016-04-04 19:30:57 +0000

    First commit for property-finder

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+   
+   def home
+       
+   end
+   
+   def about
+       
+   end
+   
+   def agents
+       
+   end
+   
+   def listings
+       
+   end
+   
+   def rentals
+       
+   end
+    
+end
