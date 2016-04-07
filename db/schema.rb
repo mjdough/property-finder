88298ee6625e7f2af419df78f6194db7a2e59508
docmj <doughmj@outlook.com>
2016-04-07 11:23:33 +0000

    add user_id to listings, login, logout links to home

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406190407) do
+ActiveRecord::Schema.define(version: 20160407111802) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "mlscode"
     t.text     "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
